fcc42c452cb3caf6f0bc76a28f91eb191867b231
andygout <andygout@hotmail.co.uk>
2015-05-13 19:24:42 +0100

    Shots fired and received

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -16,13 +16,23 @@ set :views, Proc.new { File.join(root, "..", "views") }
   end
 
   post '/game' do
-    @name = params[:name]
-    @board = @@game.own_board_view @@game.player_1
-    if @name.empty?
+    $name = params[:name]
+    if $name.empty?
       redirect '/game/new'
     else
+      @@game.player_1.place_ship Ship.battleship, 'A1', :vertically
+      @board1 = @@game.own_board_view @@game.player_1
+      @board2 = @@game.opponent_board_view @@game.player_2
       erb :start_game
     end
+  end
+
+  post '/fire' do
+    @coord = params[:coord]
+    @@game.player_2.shoot @coord.to_sym
+    @board1 = @@game.own_board_view @@game.player_1
+    @board2 = @@game.opponent_board_view @@game.player_2
+    erb :start_game
   end
 
   # start the server if ruby file executed directly
