7116431746d41c3f6d2b90d772635b02f6ead912
andygout <andygout@hotmail.co.uk>
2015-05-13 22:34:04 +0100

    Working game

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -5,34 +5,61 @@ class BattleshipsWeb < Sinatra::Base
 set :views, Proc.new { File.join(root, "..", "views") }
 
   @@game = Game.new Player, Board
-  @board = @@game.own_board_view @@game.player_1
 
   get '/' do
     erb :index
   end
 
-  get '/game/new' do
-    erb :new_game
+  get '/game/register_players' do
+    erb :register_players
   end
 
   post '/game' do
-    $name = params[:name]
-    if $name.empty?
+    $p1_name = params[:p1_name]
+    $p2_name = params[:p2_name]
+    if $p1_name.empty? && $p2_name.empty?
       redirect '/game/new'
     else
-      @@game.player_1.place_ship Ship.battleship, 'A1', :vertically
+      @@game.player_1.place_ship Ship.destroyer, 'A1', :vertically
+      @@game.player_2.place_ship Ship.destroyer, 'J1', :vertically
       @board1 = @@game.own_board_view @@game.player_1
-      @board2 = @@game.opponent_board_view @@game.player_2
-      erb :start_game
+      @board2 = @@game.opponent_board_view @@game.player_1
+      @board3 = @@game.own_board_view @@game.player_2
+      @board4 = @@game.opponent_board_view @@game.player_2
+      erb :player_1_turn
     end
   end
 
-  post '/fire' do
+  post '/game/player_1_turn' do
     @coord = params[:coord]
     @@game.player_2.shoot @coord.to_sym
-    @board1 = @@game.own_board_view @@game.player_1
-    @board2 = @@game.opponent_board_view @@game.player_2
-    erb :start_game
+    if !@@game.player_1.all_ships_sunk?
+      @board1 = @@game.own_board_view @@game.player_1
+      @board2 = @@game.opponent_board_view @@game.player_1
+      @board3 = @@game.own_board_view @@game.player_2
+      @board4 = @@game.opponent_board_view @@game.player_2
+      erb :player_1_turn
+    else
+      @player_1_win_status = @@game.player_1.winner?
+      @player_2_win_status = @@game.player_2.winner?
+      erb :end_game
+    end
+  end
+
+  post '/game/player_2_turn' do
+    @coord = params[:coord]
+    @@game.player_1.shoot @coord.to_sym
+    if !@@game.player_2.all_ships_sunk?
+      @board1 = @@game.own_board_view @@game.player_1
+      @board2 = @@game.opponent_board_view @@game.player_1
+      @board3 = @@game.own_board_view @@game.player_2
+      @board4 = @@game.opponent_board_view @@game.player_2
+      erb :player_2_turn
+    else
+      @player_1_win_status = @@game.player_1.winner?
+      @player_2_win_status = @@game.player_2.winner?
+      erb :end_game
+    end
   end
 
   # start the server if ruby file executed directly
