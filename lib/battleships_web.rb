a99e9339edbc4be1238d64c30c7282a5ceda246d
andygout <andygout@hotmail.co.uk>
2015-05-15 09:26:58 +0100

    Error msgs working using sessions

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -16,6 +16,8 @@ enable :sessions
   end
 
   post '/game/commence_battle' do
+    session[:p1_coord_error] = nil
+    session[:p2_coord_error] = nil
     session[:p1_name] = params[:p1_name]
     session[:p2_name] = params[:p2_name]
     if session[:p1_name].length < 1 || session[:p2_name].length < 1
@@ -31,6 +33,7 @@ enable :sessions
     @p1_board_own = @@game.own_board_view @@game.player_1
     @p1_board_opp = @@game.opponent_board_view @@game.player_1
     @p1_name = session[:p1_name]
+    @p1_coord_error = session[:p1_coord_error]
     erb :player_1_turn
   end
 
@@ -39,10 +42,12 @@ enable :sessions
     begin
       @result =  @@game.player_1.shoot @coord.to_sym
     rescue RuntimeError
+      session[:p1_coord_error] = "YOU HAVE ALREADY FIRED AT THESE COORDINATES: #{@coord}"
       redirect '/game/player_1_turn'
     end
     @p1_name = session[:p1_name]
     @p2_name = session[:p2_name]
+    session[:p1_coord_error] = nil
     if !@@game.player_2.all_ships_sunk?
       erb :player_1_result
     else
@@ -56,6 +61,7 @@ enable :sessions
     @p2_board_own = @@game.own_board_view @@game.player_2
     @p2_board_opp = @@game.opponent_board_view @@game.player_2
     @p2_name = session[:p2_name]
+    @p2_coord_error = session[:p2_coord_error]
     erb :player_2_turn
   end
 
@@ -64,10 +70,12 @@ enable :sessions
     begin
       @result =  @@game.player_2.shoot @coord.to_sym
     rescue RuntimeError
+      session[:p2_coord_error] = "YOU HAVE ALREADY FIRED AT THESE COORDINATES: #{@coord}"
       redirect '/game/player_2_turn'
     end
     @p1_name = session[:p1_name]
     @p2_name = session[:p2_name]
+    session[:p2_coord_error] = nil
       if !@@game.player_1.all_ships_sunk?
       erb :player_2_result
     else
